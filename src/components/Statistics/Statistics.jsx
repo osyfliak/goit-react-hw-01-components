c3e61e63c32c979337bf00deb514e75bb771270c
Osyfliak Ivan <osuflak22@gmail.com>
2023-01-31 17:01:54 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -12,13 +12,14 @@ const Statistics = ({ title, stats }) => {
 
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.stat_list}>{elements}</ul>
     </section>
   );
 };
 
 Statistics.propTypes = {
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       percentage: PropTypes.number.isRequired,
